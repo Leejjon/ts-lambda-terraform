7b38321b8cbac0cf44e415ecd954968fc002f6c4
Leejjon <leejjon@gmail.com>
2023-06-28 01:25:34 +0200

    Added the types to the typescript lambda function.

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -1,10 +1,13 @@
+import {APIGatewayProxyEventV2, APIGatewayProxyResultV2} from "aws-lambda";
+
 export const handler = async (
-    event: any
-): Promise<any> => {
+    event: APIGatewayProxyEventV2
+): Promise<APIGatewayProxyResultV2<string>> => {
     const message = "Hello World!";
+    console.log(`The event: ${JSON.stringify(event)}`);
     console.log(`Returning ${message}`);
     return {
         statusCode: 200,
-        body: JSON.stringify(message)
-    }
+        body: JSON.stringify(message),
+    } as APIGatewayProxyResultV2<string>;
 }
